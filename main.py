8153e15dd56e620e26175c2c6563c387e443c3aa
sheicksen <heicksen@udel.edu>
2024-03-02 13:07:21 -0500

    Created project files

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<p> Hello World </p>"
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run()
 

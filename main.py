7608275939f11bdcaa2852d5ba5dd102fece1131
sheicksen <heicksen@udel.edu>
2024-03-02 15:07:26 -0500

    Updated static pages and added pfp

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
 from flask import Flask, redirect, url_for, render_template, request
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
-
-# Replace the placeholder with your Atlas connection string
-# uri = "<connection string>"
-# Set the Stable API version when creating a new client
-# client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
 
 app = Flask(__name__)
 

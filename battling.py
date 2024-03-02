8b814e425180f1dd21d841a15a71c0fe7ad3adb7
sptherose <sptherose@gmail.com>
2024-03-02 15:22:40 -0500

    character class

--- battling.py
+++ battling.py
@@ -1,14 +1,20 @@
 import pygame
-class character(pygame.sprite.Sprite):
+
+class Character(pygame.sprite.Sprite):
     """Returns a character whose stats correspond with what's been unlocked by the player"""
+    hp:int
+    cp:int
+    def __init__(self,pos_x,pos_y):
+        super().__init__()
+        self.sprite = []
+        pos_x:int
+        pos_y:int
+
+class Monster(pygame.sprite.Sprite):
     hp:int
     cp:int
     def __init__(self):
         super().__init__()
-        self.sprites = []
+        self.sprite = []
 
-class monster(pygame.sprite.Sprite):
-    hp:int
-    cp:int
-    def __init__(self):
-        super()
+

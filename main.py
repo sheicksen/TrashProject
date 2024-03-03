2c8d7f4eb4d4c1665a09773b568f42243df41b27
sheicksen <heicksen@udel.edu>
2024-03-03 09:31:04 -0500

    Created xp claiming system.

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ super_secret = os.urandom(24)
 
 @dataclass
 class Monster:
-    id: uuid
+    id: str
     name:str
     size:int
     coordinates: str
     image_url: str
     date_added: str
+    quest_id: str
 
 
 @dataclass
@@ -32,7 +33,7 @@ class Character:
 ## Currently not using any of the email/password/authentication attributes
 @dataclass
 class User:
-    id: uuid
+    id: str
     email: str
     name: str
     is_authenticated: bool
@@ -45,13 +46,14 @@ class User:
     victories: []
 
     def get_id(self) -> str:
-        return str(self.id)
+        return self.id
 
 
 @dataclass
 class Quest:
-    id: uuid
+    id: str
     active: bool
+    date_started: str
     party: []
     monster: Monster
 
@@ -60,7 +62,7 @@ def create_character() -> Character:
     return Character(level=0, money=0, picture="images/blankPFP.png")
 
 def create_user(email:str, password_hash:str, name:str, image_url:str) -> User:
-    return User(id=uuid.uuid4(),
+    return User(id=str(uuid.uuid4()),
                 name=name,
                 email=email,
                 is_active=True,
@@ -75,35 +77,53 @@ def create_user(email:str, password_hash:str, name:str, image_url:str) -> User:
 
 
 def create_monster(name:str, location:str, size:int, image_url:str) -> Monster:
-    return Monster(id=uuid.uuid4(), name=name, coordinates=location, size=size, image_url=image_url, date_added=str(datetime.date.today()))
+    return Monster(id=str(uuid.uuid4()), quest_id="None", name=name, coordinates=location, size=size, image_url=image_url, date_added=str(datetime.date.today()))
 
 
 def create_quest(monster:Monster, user:User) -> Quest:
-    return Quest(id=uuid.uuid4(), active=True, party=[user], monster=monster)
+    return Quest(id=str(uuid.uuid4()), active=True, party=[user], monster=monster, date_started=str(datetime.date.today()))
 
-monsters = [Monster(uuid.uuid4(), "Gup", 5, "39.698488, -75.689280", "images/blankPFP.png", "02/03/2024"),
-            Monster(uuid.uuid4(), "Gip", 2, "39.698488, -75.689280", "images/blankPFP.png", "02/02/2024"),
-            Monster(uuid.uuid4(), "Geep", 3, "39.698488, -75.689280", "images/blankPFP.png", "03/31/2024")]
+# FAKE DB
 
-# This is the dummy user, representing the user that would normally have this browser session
+
+monsters = [Monster(str(uuid.uuid4()), "Gup", 5, "39.698488, -75.689280", "images/blankPFP.png", "02/03/2024", "None"),
+            Monster(str(uuid.uuid4()), "Gip", 2, "39.698488, -75.689280", "images/blankPFP.png", "02/02/2024", "None"),
+            Monster(str(uuid.uuid4()), "Geep", 3, "39.698488, -75.689280", "images/blankPFP.png", "03/31/2024", "None")]
+
+## This is the dummy user, representing the user that would normally have this browser session
 users = [
     create_user(name="Lorem Ipsum", email="lorem@email.com", password_hash="XDFEAFAGAG45343", image_url="images/blankPFP.png")
 ]
-users[0].current_battles.append(create_quest(monsters[0], users[0]))
 
+quests = []
 
-def find_user_by_id(id:uuid, users:[]) -> User:
+def find_user_by_id(id:str, users:[]) -> User:
+    found_user = create_user(name="User Not Found", email="", password_hash="", image_url=url_for('static', filename="images/blankPFP.png"))
     for user in users:
-        if id == user.id:
-            return user
-    return create_user(name="User Not Found", image_url=url_for('static', filename="images/blankPFP.png"))
+        if (id == user.id):
+            found_user=user
+    return found_user
 
 
 def find_user_by_name(name:str, users:[]) -> User:
     for user in users:
         if name == user.name:
             return user
-    return create_user(name="User Not Found", image_url=url_for('static', filename="images/blankPFP.png"))
+    return create_user(name="Not Found", image_url=url_for('static', filename="images/blankPFP.png"))
+
+
+def find_monster_by_id(id:str, monsters:[]) -> Monster:
+    found_monster = create_monster(name="Not Found", location="Nowhere", size=0, image_url=url_for('static', filename="images/blankPFP.png"))
+    for monster in monsters:
+        if id == monster.id:
+            found_monster = monster
+    return found_monster
+
+def find_quest_by_id(id:str, quests:[]) -> Quest:
+    for quest in quests:
+        if id == quest.id:
+            return quest
+    return Quest(id=0, party=[], monster=create_monster(name="Not Found", location="Nowhere", size=0, image_url=url_for('static', filename="images/blankPFP.png")), active=False )
 
 
 def allowed_file(filename) -> bool:
@@ -119,7 +139,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def home():
-    return render_template("index.html", monsters=monsters)
+    return render_template("index.html", monsters=monsters, user=users[0])
 
 @app.route("/about")
 def get_about():
@@ -163,12 +183,49 @@ def get_profile(username):
     else:
         return render_template("profile.html",
                                username=user.name,
-                               victories=len(user.victories),
+                               user_id=user.id,
+                               victories=user.victories,
+                               monsters_def = len(user.victories),
                                quests= user.current_battles,
                                level=user.character.level,
                                profile=user.profile_picture,
                                character=user.character.picture
                                )
+
+@app.route("/add_quest")
+def add_quest():
+    query = request.args.getlist('ids')
+    monster= find_monster_by_id(query[0], monsters)
+    user = find_user_by_id(query[1], users)
+
+    if monster.name == "Not Found" or user.name == "Not Found":
+        print("Either user or monster does not exist")
+        return redirect(url_for("home"))
+    else:
+        for quest in user.current_battles:
+            if quest.monster.id == monster.id:
+                print("User already created quest")
+                return redirect(url_for('get_profile', username=user.name))
+        new_quest = create_quest(monster=monster, user=user)
+        quests.append(new_quest)
+        user.current_battles.append(new_quest)
+        monster.quest_id = new_quest.id
+        return redirect(url_for('get_profile', username=user.name))
+
+@app.route("/claim_xp")
+def mark_complete():
+    query = request.args.getlist('ids')
+    quest= find_quest_by_id(query[0], quests)
+    user = find_user_by_id(query[1], users)
+    if user.name == "Not Found" or quest.id == 0:
+        return redirect(url_for("home", user=users[0]))
+    else:
+        quest.active = False
+        user.character.level += 5 * quest.monster.size
+        user.current_battles.remove(quest)
+        user.victories.append(quest)
+        return redirect(url_for("get_profile", username=user.name))
+
 
 if __name__ == '__main__':
     app.secret_key = super_secret
